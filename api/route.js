c74f46451c6896f379d79d50923233f73cbc9d07
Xpectedlol <JOjooakpark@gmail.com>
2025-08-07 13:55:03 -0500

    popular

--- api/route.js
+++ api/route.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const search = require('./search');
+const popular = require('./popular');
 
 router.get('/sneakers/:sneaker', search);
+router.get('/popular/', popular);
 
 module.exports = router;
