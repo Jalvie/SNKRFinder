116aa214f72ab86d0533f21d8b49dab560696990
Xpectedlol <JOjooakpark@gmail.com>
2025-08-08 12:18:06 -0500

    favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,13 @@
 </script>
 
 <svelte:head>
+	<!-- Favicons and Manifest -->
+	<link rel="icon" type="image/png" href="/assets/favicon/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/assets/favicon/favicon.svg" />
+	<link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
+	<meta name="apple-mobile-web-app-title" content="SNKR Finder" />
+	<link rel="manifest" href="/assets/favicon/site.webmanifest" />
 	<!-- Google tag (gtag.js) -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=G-W5DRVX27EE"></script>
 	<script>
